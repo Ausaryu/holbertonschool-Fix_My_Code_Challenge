a3fa35e821f22f7942b139449ab8088e18518825
Ausaryu <yonas.houriez@gmail.com>
2026-06-01 10:36:44 +0200

    Implement functions for adding, deleting, and freeing nodes in a doubly linked list

---
+++ delete_dnodeint_at_index.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ *
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current, *to_delete;
+	unsigned int i;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	current = *head;
+
+	if (index == 0)
+	{
+		to_delete = *head;
+		*head = to_delete->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(to_delete);
+		return (1);
+	}
+
+	for (i = 0; current != NULL && i < index - 1; i++)
+		current = current->next;
+
+	if (current == NULL || current->next == NULL)
+		return (-1);
+
+	to_delete = current->next;
+	current->next = to_delete->next;
+
+	if (to_delete->next != NULL)
+		to_delete->next->prev = current;
+
+	free(to_delete);
+
+	return (1);
+}
